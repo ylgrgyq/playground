c76c7ec14785683f7941ac3450b663ec397386e4
ylgrgyq <alven.gr@gmail.com>
2018-10-28 14:25:24 +0800

    (feat) support let fun

--- monkey-interpreter/src/lexer/lexer.go
+++ monkey-interpreter/src/lexer/lexer.go
@@ -1,6 +1,8 @@
 package lexer
 
-import "token"
+import (
+	"token"
+)
 
 type Lexer struct {
 	input        string
@@ -14,22 +16,9 @@ func New(input string) *Lexer {
 	return l
 }
 
-func (l *Lexer) readChar() {
-	if l.readPosition >= len(l.input) {
-		l.ch = 0
-	} else {
-		l.ch = l.input[l.readPosition]
-	}
-	l.position = l.readPosition
-	l.readPosition++
-}
-
-func newToken(tokenType token.TokenType, ch byte) token.Token {
-	return token.Token{Type: tokenType, Literal: string(ch)}
-}
-
 func (l *Lexer) NextToken() token.Token {
 	l.readChar()
+	l.skipWhiteSpaces()
 
 	var tok token.Token
 	switch l.ch {
@@ -52,7 +41,71 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			switch tok.Literal {
+			case "let":
+				tok.Type = token.LET
+			case "fn":
+				tok.Type = token.FUNCTION
+			default:
+				tok.Type = token.IDENT
+			}
+		} else if isNumber(l.ch) {
+			tok.Literal = l.readInt()
+			tok.Type = token.INT
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	return tok
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+func (l *Lexer) skipWhiteSpaces() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch == '_')
+}
+
+func (l *Lexer) readIdentifier() string {
+	for isLetter(l.input[l.readPosition]) {
+		l.readPosition++
+	}
+	ident := l.input[l.position:l.readPosition]
+	l.position = l.readPosition - 1
+	return ident
+}
+
+func isNumber(ch byte) bool {
+	return ch >= '0' && ch <= '9'
+}
+
+func (l *Lexer) readInt() string {
+	for isNumber(l.input[l.readPosition]) {
+		l.readPosition++
+	}
+
+	n := l.input[l.position:l.readPosition]
+	l.position = l.readPosition - 1
+	return n
 }
