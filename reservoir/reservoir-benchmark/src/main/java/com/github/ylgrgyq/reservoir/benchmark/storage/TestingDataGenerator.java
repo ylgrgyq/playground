b4f99a5bdbaef93243a42fdc38c53e8021d0d4d2
ylgrgyq <alven.gr@gmail.com>
2019-09-09 16:11:33 +0800

    (fix) test

--- reservoir/reservoir-benchmark/src/main/java/com/github/ylgrgyq/reservoir/benchmark/storage/TestingDataGenerator.java
+++ reservoir/reservoir-benchmark/src/main/java/com/github/ylgrgyq/reservoir/benchmark/storage/TestingDataGenerator.java
@@ -3,6 +3,8 @@ package com.github.ylgrgyq.reservoir.benchmark.storage;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.github.ylgrgyq.reservoir.benchmark.storage.TestingUtils.makeStringInBytes;
+
 final class TestingDataGenerator {
     static List<List<byte[]>> generate(int dataSize, int dataCount, int batchSize) {
         final List<List<byte[]>> testingData = new ArrayList<>();
@@ -18,7 +20,7 @@ final class TestingDataGenerator {
     static List<byte[]> generateBatch(int batchSize, int dataSize) {
         final List<byte[]> batch = new ArrayList<>();
         for (int i = 0; i < batchSize; i++) {
-            batch.add(TestingUtils.makeStringInBytes("Hello", dataSize));
+            batch.add(makeStringInBytes("Hello", dataSize));
         }
         return batch;
     }
